2899163b5f2504def7743bc3e5f346c4fada4d13
hainam9216 <hainam9216@gmail.com>
2024-01-19 23:33:49 +0700

    change method

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import TryGlasses from './tryGlasses';
-import Header from './tryGlasses/header';
-import Model from './tryGlasses/model';
-import Glasses from './tryGlasses/glasses';
+import Header from './tryGlasses/header'
 
 function App() {
   return (
     <>
     <Header/>
-    <Model/>
     <TryGlasses/>
-    <Glasses/>
     </>
   );
 }
